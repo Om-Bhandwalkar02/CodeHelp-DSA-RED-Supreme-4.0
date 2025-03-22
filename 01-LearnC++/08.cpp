5373c3f6de57ddd55fc4a6c96616567bd67b919f
Om-Bhandwalkar02 <ombhandwalkar@outlook.com>
2025-03-22 15:29:10 +0000

    Do-while && nested loop

---
+++ 01-LearnC++/08.cpp
@@ -0,0 +1,35 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+
+   int i = 0;
+   do{
+       cout << i << " ";
+       i++;
+   }while(i<=5);
+   cout << endl;
+
+if(cout << "hi"){
+    cout << "Hello";
+}
+
+cout << endl;
+
+// int a;
+// if(cin >> a){
+//     cout << "Hello";
+// }
+
+cout << endl;
+
+for(int i=1; i<=3; i++){
+    for(int j=1; j<=3; j++){
+        cout << i << j << " ";
+    }
+    cout << endl;
+}
+
+
+    return 0;
+}
