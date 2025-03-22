57af056e47f32df79fa35a44db048f588be34b28
Om-Bhandwalkar02 <ombhandwalkar@outlook.com>
2025-03-22 13:38:01 +0000

    Control Flow

---
+++ 01-LearnC++/04.cpp
@@ -0,0 +1,82 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+
+    int budget;
+    // cout << "Enter your budget: ";
+    // cin >> budget;
+
+    // if(budget > 2000000){
+    //     cout << "You can buy scorpio" << endl;
+    // }
+    // else{
+    //     cout << "You cannot buy scorpio" << endl;
+    // }
+
+
+
+
+
+
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // if(age >= 18){
+    //     cout << "You can vote" << endl;
+    // }
+    // else{
+    //     cout << "You cannot vote" << endl;
+    // }
+
+
+
+
+
+    // int marks = 90;
+
+    // if(marks >= 90){
+    //     cout << "Grade A" << endl;
+    // }
+    // else if(marks >= 80){
+    //     cout << "Grade B" << endl;
+    // }
+    // else if(marks >= 70){
+    //     cout << "Grade C" << endl;
+    // }
+    // else if(marks >= 60){
+    //     cout << "Grade D" << endl;
+    // }
+    // else{
+    //     cout << "Grade F" << endl;
+    // }
+
+
+
+
+
+    int height;
+    cout << "Enter your height: ";
+
+    cin >> height;
+
+    int weight;
+    cout << "Enter your weight: ";
+
+    cin >> weight;
+
+
+    if(height >= 6){
+        if(weight >= 70){
+            cout << "You are fit" << endl;
+        }
+        else{
+            cout << "Bhaiii tuz se naa hoo paegaa" << endl;
+        }
+    }
+    else{
+        cout << "Complain dila du!" << endl;
+    }
+    return 0;
+}
